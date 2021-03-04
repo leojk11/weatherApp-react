54e48d786af86599cece57a112d535bf2a5f4449
Leonid Jakimovski <ljakimovski211@yahoo.com>
2021-03-04 23:49:50 +0100

    Added header component
    Added header component with icon with should be like logo, title, and search input

--- src/components/Header/header.component.js
+++ src/components/Header/header.component.js
@@ -3,10 +3,24 @@ import React from 'react';
 // css
 import './header.component.css';
 
+// components
+import CloudIcon from '@material-ui/icons/Cloud';
+import SearchIcon from '@material-ui/icons/Search';
+
 const Header = () => {
     return (
         <div className='header'>
-            <div className="logo"></div>
+            <div className="header__wrapper">
+                <div className="logo">
+                    <CloudIcon style={{ color: '#fff' }} />
+                    <h2>Weather</h2>
+                </div>
+
+                <div className="search">
+                    <SearchIcon style={{ color: '#000000' }} />
+                    <input type="text" placeholder='Search' />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/data/weather.js
@@ -0,0 +1,11 @@
+export const weatherData = {
+    tempUnit: 'C',
+    windSpeedUnit: 'm/s',
+    days: [
+        { day: 'Mon', temp: 22, lowestTemp: 12, windDirection: 'north-east', windSpeed: 10, type: 'sunny', airQality: 'good' },
+        { day: 'Tue', temp: 14, lowestTemp: 2, windDirection: 'north-west', windSpeed: 14, type: 'rainy', airQality: 'bad' },
+        { day: 'Wed', temp: 17, lowestTemp: 11, windDirection: 'south-east', windSpeed: 20, type: 'cloudy', airQality: 'good' },
+        { day: 'Thu', temp: 20, lowestTemp: 9, windDirection: 'north-east', windSpeed: 3, type: 'sunny', airQality: 'good' },
+        { day: 'Fri', temp: 12, lowestTemp: -1, windDirection: 'north-west', windSpeed: 5, type: 'cloudy', airQality: 'bad' }
+    ]
+};

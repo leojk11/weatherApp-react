dad7d542102a39875221f9b74e9f274c5b62f27e
Leonid Jakimovski <ljakimovski211@yahoo.com>
2021-03-04 23:00:58 +0100

    first commit
    Commiting the start of the project.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// css
+import './App.css';
+
+// components
+import Header from './components/Header/header.component';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/header.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// css
+import './header.component.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="logo"></div>
+        </div>
+    )
+}
+
+export default Header;
